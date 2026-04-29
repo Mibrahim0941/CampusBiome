4fa430c1196a83faa6b6ed8051e6a8689d91c128
Mibrahim0941 <mi094131@gmail.com>
2026-04-30 00:52:51 +0500

    Update GenericLoginActivity.java

--- app/src/main/java/com/example/campusbiome/GenericLoginActivity.java
+++ app/src/main/java/com/example/campusbiome/GenericLoginActivity.java
@@ -118,21 +118,27 @@ public class GenericLoginActivity extends AppCompatActivity {
     }
 
     private void goToDashboard() {
+        Intent intent;
         if(role.equals("society_manager")){
 
-            Intent intent = new Intent(this, SocietyDashboardActivity.class);
+            intent = new Intent(this, SocietyDashboardActivity.class);
             intent.putExtra("role", role);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
         }
-        else{
-            Intent intent = new Intent(this, Dashboard.class);
+        else if("admin".equals(role)){
+            intent = new Intent(this, AdminDashboardActivity.class);
+            intent.putExtra("role", role);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
+        else {
+            intent = new Intent(this, FacultyDashboardActivity.class);
             intent.putExtra("role", role);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
         }
     }
-
     private void onFailed(String message) {
         Toast.makeText(this, "Authentication failed: " + message, Toast.LENGTH_LONG).show();
     }
